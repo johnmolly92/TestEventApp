9296d4586e3bfa960c355e06998aa48a9746bfae
John Molloy <John@Johns-MacBook-Pro.local>
2015-02-24 19:51:43 +0000

    First push to Git
    Uploading app for first time. Login and Register implemented.

---
+++ app/src/main/java/com/example/john/testeventapp/MainActivity.java
@@ -0,0 +1,189 @@
+package com.example.john.testeventapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.ProgressDialog;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.util.Log;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity implements OnClickListener{
+
+    private EditText user, pass;
+    private Button mSubmit, mRegister;
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+
+    //php login script location:
+
+    //localhost :
+    //testing on your device
+    //put your local ip instead,  on windows, run CMD > ipconfig
+    //or in mac's terminal type ifconfig and look for the ip under en0 or en1
+    private static final String LOGIN_URL = "http://136.206.236.53:80/webservice/login.php";
+
+    //testing on Emulator:
+    //private static final String LOGIN_URL = "http://10.0.2.2:1234/webservice/login.php";
+
+    //testing from a real server:
+    //private static final String LOGIN_URL = "http://www.yourdomain.com/webservice/login.php";
+
+    //JSON element ids from repsonse of php script:
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setup input fields
+        user = (EditText)findViewById(R.id.txtUsername);
+        pass = (EditText)findViewById(R.id.txtPassword);
+
+        //setup buttons
+        mSubmit = (Button)findViewById(R.id.btnLogin);
+        mRegister = (Button)findViewById(R.id.btnRegister);
+
+        //register listeners
+        mSubmit.setOnClickListener(this);
+        mRegister.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // determine which button was pressed:
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                new AttemptLogin().execute();
+                break;
+            case R.id.btnRegister:
+                Intent i = new Intent(this, Register.class);
+                startActivity(i);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    //AsyncTask is a seperate thread than the thread that runs the GUI
+    //Any type of networking should be done with asynctask.
+    class AttemptLogin extends AsyncTask<String, String, String> {
+
+        //three methods get called, first preExecture, then do in background, and once do
+        //in back ground is completed, the onPost execture method will be called.
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Attempting login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+
+            // Check for success tag
+            int success;
+            String username = user.getText().toString();
+            String password = pass.getText().toString();
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL, "POST", params);
+
+                // check your log for json response
+                Log.d("Login attempt", json.toString());
+
+                // json success tag
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Login Successful!", json.toString());
+                    Intent i = new Intent(MainActivity.this, Home.class);
+                    finish();
+                    startActivity(i);
+                    return json.getString(TAG_MESSAGE);
+                }else{
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null){
+                Toast.makeText(MainActivity.this, file_url, Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+}
